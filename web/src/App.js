d2ad4adf3d85cc1509098f63a8a6f7daafa90113
willian stein <willianinline@gmail.com>
2020-03-20 20:55:37 -0300

    primeiros ajustes

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <h1>hello</h1>
+    <div id="app">
+ <aside></aside>
+ <main>
+
+ </main>
+ </div>
   );
 }
 
